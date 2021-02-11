4eebbb42385a3ac907d6ee2d5ce41d0767dab8ea
Juni Weisteen Bjerde <juni.w.bjerde@gmail.com>
2021-02-11 23:38:55 +0100

    add agenda-component

---
+++ components/Agenda.js
@@ -0,0 +1,24 @@
+import { render } from 'react-dom';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+const Agenda = (props) => {
+    const agenda = props.agenda;
+    return (
+        <VerticalTimeline>
+            {agenda.map( event => {
+                return (
+                    <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date={event.time}
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+                        <h3>{event.title}</h3>
+                        <p>{event.description}</p>
+                    </VerticalTimelineElement>);
+            })}
+        </VerticalTimeline>
+    )
+};
+export default Agenda;
--- pages/api/state/index.js
+++ pages/api/state/index.js
@@ -1,6 +1,12 @@
 const handler = (req, res) => {
   // fetch agenda from mongodb
   // fetch lines from mongog
-  res.status(200).json({ agenda: [{time: "18:00", text: "Noe som skjer"}], lines: ["line1", "line2"] });
+  res.status(200).json(
+    { agenda: [
+      {time: "18.00", title: "event1", description: "dette er en ting som skal skje"}, 
+      {time: "13.00", title: "event3", description: "dette er en annen ting som skal skje"},
+      {time: "17.00", title: "event2", description: "blablabla"},
+      {time: "20.00", title: "event4", description: "hei ho"},
+    ]});
 }
 export default handler;
