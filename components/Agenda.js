f705689006421a84c9d9d55e61de6f249bed0537
peterjfb <peterj.flbj@gmail.com>
2021-02-13 14:28:05 +0100

    make agenda less imposing

--- components/Agenda.js
+++ components/Agenda.js
@@ -9,18 +9,31 @@ import styles from './Agenda.module.css';
 const Agenda = (props) => {
   const agenda = props.agenda;
   return (
-    <VerticalTimeline className={styles.space}>
+    <VerticalTimeline>
       {agenda.map((event) => {
         return (
+          //rgb(33, 150, 243)
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            className={styles.verticalTimelineElementWork}
+            contentStyle={{
+              background: '#ffffff',
+              color: '#fff',
+              boderTop: '7px solid black',
+            }}
+            contentArrowStyle={{ borderRight: '7px solid  #ffffff' }}
             date={event.time}
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            dateClassName={styles.verticalTimelineElementDate}
+            iconStyle={{
+              background: 'rgb(33, 150, 243)',
+              color: '#fff',
+            }}
           >
-            <h3>{event.title}</h3>
-            <p>{event.description}</p>
+            <h3 className={styles.verticalTimelineElementTitle}>
+              {event.title}
+            </h3>
+            <p className={styles.verticalTimelineElementDescription}>
+              {event.description}
+            </p>
           </VerticalTimelineElement>
         );
       })}
