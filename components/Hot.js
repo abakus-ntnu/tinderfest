3a5c1c2b348c9a614fd60782e5afec722be46af1
peterjfb <peterj.flbj@gmail.com>
2021-02-13 10:30:41 +0100

    clean up code in hot/not buttons

--- components/Hot.js
+++ components/Hot.js
@@ -1,17 +1,19 @@
 import { useEffect } from "react";
 import styles from "./Hot.module.css";
 
-const sendHot = () => {
-  fetch(`${process.env.NEXT_PUBLIC_API_URL}/hot`, {
-    method: "POST",
-  })
-    .catch((error) => {
-      console.error(error);
-    });
-};
 
 const Hot = (props) => {
-  const socket = props.socket;
+  //const socket = props.socket;
+  
+  const sendHot = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/hot`, {
+      method: "POST",
+    })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
   return (
     <div className={styles.button} onClick={sendHot}>
       <img className={styles.heart} src="/hot.png" alt="hot"/>
