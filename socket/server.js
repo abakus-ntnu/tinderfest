2afc72e0c7a15ea397107b92a98f843c2a537e76
Peder Smith <smith.peder@gmail.com>
2021-02-13 18:38:00 +0100

    Bump limit

--- socket/server.js
+++ socket/server.js
@@ -13,14 +13,13 @@ const RateLimit = require('express-rate-limit');
 const RedisStore = require('rate-limit-redis');
 const env = require('./env');
 
-console.log('ENV', env.REDIS_URL);
 const apiLimiter = new RateLimit({
   store: new RedisStore({
     redisURL: env.REDIS_URL,
     expiry: 1, // 2 seconds
   }),
   // windowMs: 1000, // 1 second (redis uses expiry, not windowMs, see https://github.com/wyattjoh/rate-limit-redis/issues/32 )
-  max: 2, // limit each IP to 3 requests per windowMs or expiry
+  max: 50, // limit each IP to 3 requests per windowMs or expiry
   statusCode: 429,
   message: 'Rate limit exceeded. Please wait',
 });
