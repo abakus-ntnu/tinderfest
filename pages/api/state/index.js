a75d03b088f4625f5880b821856fcfc855dc91b3
Peder Smith <smith.peder@gmail.com>
2021-02-13 13:31:56 +0100

    Add atlas mongodb connection

--- pages/api/state/index.js
+++ pages/api/state/index.js
@@ -4,10 +4,9 @@ import { Agenda, Info } from '../../../models/schema.js';
 
 const username = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
-const dbname = 'Tinderfest';
+const dbname = 'tinderfest';
 
-//export const url = `mongodb+srv://${username}:${password}@cluster.au8e8.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-export const url = 'mongodb://localhost/Tinderfest';
+export const url = `mongodb+srv://${username}:${password}@tinderfest.zrvrz.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 export default async function handler(_, res) {
   mongoose.connect(url, {
@@ -24,32 +23,10 @@ export default async function handler(_, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
 
-  /*
   res.end(
     JSON.stringify({
-        agenda,
-        info,
-    })
-  );
-  */
-
-  res.end(
-    JSON.stringify({
-      agenda: [
-        {
-          time: '18.00',
-          title: 'event1',
-          description: 'dette er en ting som skal skje',
-        },
-        {
-          time: '13.00',
-          title: 'event3',
-          description: 'dette er en annen ting som skal skje',
-        },
-        { time: '17.00', title: 'event2', description: 'blablabla' },
-        { time: '20.00', title: 'event4', description: 'hei ho' },
-      ],
-      info: ['gherifj<OJFAJO', 'JFOEJOFWJF', 'jorroikgekfmandkf'],
+      agenda,
+      info,
     })
   );
 }
